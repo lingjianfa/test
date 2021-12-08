889af2f9772c55fcd6d1d5af10c5183ef49f91b4
lingjianfa <lingjianfa@qq.com>
2021-12-08 18:43:42 +0800

    test

--- test.cpp
+++ test.cpp
@@ -4,108 +4,93 @@
 
 #if !defined __MINGW32__
 
-class a_exception : public std::exception {
- public:
-   virtual const char* what() const noexcept override
-   {
-      return "Test Exception";
-   }
+class a_exception : public std::exception
+{
+public:
+  virtual const char* what() const noexcept override
+  {
+    return "Test Exception";
+  }
 };
 
-void throw_a_exception()
+void
+throw_a_exception()
 {
-   throw a_exception();
+  throw a_exception();
 }
 
-void throw_a_integer(int a)
+void
+throw_a_integer(int a)
 {
-   throw a;
+  throw a;
 }
 
 SUITE(exception)
 {
-   Case(integer)
-   {
-      throw 42;
-   }
+  Case(integer) { throw 42; }
 
-   Case(catched)
-   {
-      throw_a_exception();
-   }
+  Case(catched) { throw_a_exception(); }
 }
 
 CASE(exception uncaught)
 {
-   throw_a_integer(42);
+  throw_a_integer(42);
 }
 
 CASE(exception)
 {
-   try {
-      throw_a_exception();
-   } catch (...) {
-   }
+  try {
+    throw_a_exception();
+  } catch (...) {
+  }
 }
 
 SUITE(exception catch)
 {
-   Case(no throw)
-   {
-      Catch()
-      {
-         throw_a_exception();
-      }
-   }
+  Case(no throw)
+  {
+    Catch() { throw_a_exception(); }
+  }
 
-   Case(no throw)
-   {
-      Catch(nothrow)
-      {
-         throw_a_exception();
-      }
-   }
+  Case(no throw)
+  {
+    Catch(nothrow) { throw_a_exception(); }
+  }
+
+  Case(std::out_of_range)
+  {
+    Catch()
+    {
+      std::string().at(1); // generates an std::out_of_range
+    }
+  }
 
 #if !defined _MSC_VER
 
-   Case(check throw type)
-   {
-      Catch(const char*)
-      {
-         throw_a_integer(42);
-      }
-   }
+  Case(check throw type)
+  {
+    Catch(const char*) { throw_a_integer(42); }
+  }
 
-   Case(check throw type)
-   {
-      Catch(const char*)
-      {
-      }
-   }
+  Case(check throw type)
+  {
+    Catch(const char*) {}
+  }
 
-   Case(check throw type and matcher)
-   {
-      Catch(int, 16)
-      {
-         throw_a_integer(42);
-      }
-   }
+  Case(check throw type and matcher)
+  {
+    Catch(int, 16) { throw_a_integer(42); }
+  }
 
-   Case(check throw type a_exception)
-   {
-      Catch(a_exception)
-      {
-         throw_a_exception();
-      }
-   }
+  Case(check throw type a_exception)
+  {
+    Catch(a_exception) { throw_a_exception(); }
+  }
 
-   Case(check throw type a_exception and matcher streq)
-   {
-      Catch(a_exception, "hello")
-      {
-         throw_a_exception();
-      }
-   }
+  Case(check throw type a_exception and matcher streq)
+  {
+    Catch(a_exception, "hello") { throw_a_exception(); }
+  }
 
 #endif
 }
