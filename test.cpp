8ae5c5541b540efbac97d5c109b05174d71c1eb3
lingjianfa <lingjianfa@qq.com>
2021-12-28 23:39:48 +0800

    test4

--- test.cpp
+++ test.cpp
@@ -5,12 +5,53 @@
 
 CASE(malloc faulty injection successful [pass])
 {
-   BLOCK(limit = 10)
+   // BLOCK(limit = 10)
+   // {
+   //    auto a1 = (char*)malloc(11);
+
+   //    std::cout << h2::h2_cxa::type_name<decltype(a1)>() << std::endl;
+
+   //    OK(NULL, a1);
+   // }
+
+   // h2::h2_stringify
+}
+
+SUITE()
+{
+
+   Case(char*)
    {
-      auto a1 = (char*)malloc(11);
-
-      std::cout << h2::h2_cxa::type_name<decltype(a1)>() << std::endl;
-
-      OK(NULL, a1);
+      char* a = (char*)"hello world";
+      OK("hello world", h2::h2_stringify<char*>(a));
+      OK(ListOf("\033{+dark gray}", "\"", "\033{-dark gray}",
+                "hello world",
+                "\033{+dark gray}", "\"", "\033{-dark gray}"),
+         h2::h2_stringify<char*>(a, true));
    }
+
+   Case("char[]")
+   {
+      char a[100];
+      strcpy(a, "hello world");
+
+      OK("hello world", h2::h2_stringify(a));
+      OK(ListOf("\033{+dark gray}", "\"", "\033{-dark gray}",
+                "hello world",
+                "\033{+dark gray}", "\"", "\033{-dark gray}"),
+         h2::h2_stringify(a, true));
+   }
+
+   Case(char* null)
+   {
+      char* a = nullptr;
+      OK(ListOf("(null)"), h2::h2_stringify<char*>(a));
+   }
+
+   Case(const char* null)
+   {
+      const char* a = nullptr;
+      OK(ListOf("(null)"), h2::h2_stringify<const char*>(a));
+   }
+
 }
